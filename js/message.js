fd9ffcb7f654b531ec9a0a3e59e596f4cba2fe8e
martin root <martinrootii@outlook.com>
2018-10-18 22:06:58 +0800

    添加留言功能

--- js/message.js
+++ js/message.js
@@ -1,18 +1,48 @@
 var APP_ID = '01CXU2dGBmFbFjm0qb1rDjht-gzGzoHsz';
 var APP_KEY = 'YpjhSRMEPOmctorINAn2ERXQ';
- AV.init({
+
+AV.init({
   appId: APP_ID,
   appKey: APP_KEY
 });
 
-// 创建 TestObject 表
-var X = AV.Object.extend('Frank2');
-// 在表中创建一行数据
-var o = new X();
-// 数据内容是 words: 'Hello World!' 保存
-// 如果保存成功，则运行 alert('')
-o.save({
-  xxxxxxxxxx: 'Fuck World!'
-}).then(function() {
-  console.log(arguments[0])
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit',function(e){
+  e.preventDefault()
+  let content = myForm.querySelector('input[name=content]').value
+  let name = myForm.querySelector('input[name=name]').value
+  var Message = AV.Object.extend('Message');
+  var message = new Message();
+  message.save({
+    'name':name,
+    'content': content
+  }).then(function(object){
+    window.location.reload()
+
+  })
 })
+
+var query = new AV.Query('Message');
+query.find()
+  .then(
+      function(messages){
+        let array = messages.map((item)=>item.attributes)
+        array.forEach((item)=> {
+        let li = document.createElement('li')
+        li.innerText = `${item.name}: ${item.content}`
+        let message = document.querySelector('#messageList')
+        messageList.appendChild(li)
+      });
+    }
+  )
+
+/*
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
+*/
